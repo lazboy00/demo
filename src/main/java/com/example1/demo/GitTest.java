e5faf49ae33e44945a3c5ada1f162ff910a3739e
zhangying <zhangying@qq.com>
2022-06-21 20:26:48 +0800

    firstCommit

---
+++ src/main/java/com/example1/demo/GitTest.java
@@ -0,0 +1,14 @@
+package com.example1.demo;
+
+import java.sql.SQLOutput;
+
+/**
+ * 2022-06-21-20:23
+ * author:sun
+ * other:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
